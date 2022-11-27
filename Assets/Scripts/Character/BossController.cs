f1779babb34ba6878e99ad84cd9165620e3c0e37
AC <109703034@g.nccu.edu.tw>
2022-11-27 23:49:10 +0800

    1127

--- Assets/Scripts/Character/BossController.cs
+++ Assets/Scripts/Character/BossController.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DogController : MonoBehaviour
+public class BossController : MonoBehaviour
 {
-    public Animator dogAnime;
+    public Animator Anime;
 
     public float speed = 0.8f;
     public float dieTime = 4f;
@@ -13,65 +13,65 @@ public class DogController : MonoBehaviour
     bool isWalking = false;
     bool isDying = false;
 
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(isDying){
+        if (isDying)
+        {
             time += Time.deltaTime;
-            if(time > dieTime){
+            if (time > dieTime)
+            {
                 isDying = false;
                 time = 0;
             }
         }
-        else{
+        else
+        {
             isWalking = false;
-        
-            if (Input.GetKey(KeyCode.RightArrow)) {
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
                 isWalking = true;
                 Quaternion newRotation = Quaternion.Euler(0, 90f, 0);
                 this.gameObject.transform.rotation = newRotation;
                 this.gameObject.transform.position += this.gameObject.transform.forward * speed * Time.fixedDeltaTime;
             }
-            if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
                 isWalking = true;
                 Quaternion newRotation = Quaternion.Euler(0, -90f, 0);
                 this.gameObject.transform.rotation = newRotation;
                 this.gameObject.transform.position += this.gameObject.transform.forward * speed * Time.fixedDeltaTime;
             }
-            if (Input.GetKey(KeyCode.UpArrow)) {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
                 isWalking = true;
                 Quaternion newRotation = Quaternion.Euler(0, 0, 0);
                 this.gameObject.transform.rotation = newRotation;
                 this.gameObject.transform.position += this.gameObject.transform.forward * speed * Time.fixedDeltaTime;
             }
-            if (Input.GetKey(KeyCode.DownArrow)) {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
                 isWalking = true;
                 Quaternion newRotation = Quaternion.Euler(0, 180f, 0);
                 this.gameObject.transform.rotation = newRotation;
                 this.gameObject.transform.position += this.gameObject.transform.forward * speed * Time.fixedDeltaTime;
             }
 
-            if(isWalking){
-                dogAnime.SetInteger("Status", 1);
+            if (isWalking)
+            {
+                Anime.SetInteger("Status", 1);
             }
-            else{
-                dogAnime.SetInteger("Status", 0);
-            }
-        }
-    }
-
-    void OnTriggerEnter(Collider other){
-        if(other.gameObject.tag == "Turtle"){
-            if(!isDying){
-                isDying = true;
-                GameObject.Find("GameManager").GetComponent<GameManager>().minusLife();
-                dogAnime.SetInteger("Status", 2);
+            else
+            {
+                Anime.SetInteger("Status", 0);
             }
         }
     }
