836dbaf580dd4c87b948f2d501f4a87e05bff72a
Paul Yang <paulyang80@gmail.com>
2022-11-28 17:07:51 +0800

    Gravity Upload
    Gravity and Key

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    public GameObject panel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMessagePanel(){
+        panel.SetActive(true);
+    }
+
+    public void CloseMessagePanel(){
+        panel.SetActive(false);
+    }
+
+    
+}
