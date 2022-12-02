c1c524977fbb2b8748e2a1000ec0b5caf3ba141a
109703043 <109703043@nccu.edu.tw>
2022-12-02 23:30:15 +0800

    Network added. Lab2 env building.

---
+++ Assets/Scripts/Network/StartSceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviourPunCallbacks
+{
+    public void OnClickStart(){
+        print("[ ClickStart ]");
+        // PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(){
+        print("Connected! Joining Lobby...");
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
---
+++ Assets/Scripts/Network/WaitingSceneManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+using Text = TMPro.TextMeshProUGUI;
+
+public class WaitingSceneManager : MonoBehaviourPunCallbacks
+{
+    public Text currRoomKeyText;
+    public Text waitingPlayersText;
+    public int maxPlayerCount = 5;
+
+    void Start()
+    {
+        if(PhotonNetwork.IsConnected == false){
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+
+    void Update()
+    {
+        if(PhotonNetwork.InRoom){
+            currRoomKeyText.text = "Room Key: " + PhotonNetwork.CurrentRoom.Name;
+            waitingPlayersText.text = PhotonNetwork.CurrentRoom.PlayerCount + "/" + maxPlayerCount + " are waiting...";
+            if(PhotonNetwork.CurrentRoom.PlayerCount == maxPlayerCount){
+                Player bossPlayer = PhotonNetwork.CurrentRoom.Players[Random.Range(1, maxPlayerCount+1)];
+                PhotonNetwork.SetMasterClient(bossPlayer);
+                print("switched to " + bossPlayer);
+                SceneManager.LoadScene("TempScene");
+            }
+        }
+    }
+
+    public void OnClickQuit(){
+        print("[ ClickQuit ]");
+        if(PhotonNetwork.InRoom){
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    public override void OnLeftRoom(){
+        print("Left Room");
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
